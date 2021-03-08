4557b8d125806ec8792cef906fa9e78e8861113a
Lukas Raitzl <lukas.raitzl@gmail.com>
2021-03-08 18:34:20 +0100

    1.0 Basic

---
+++ app/src/main/java/com/example/movieapp/Movie.kt
@@ -0,0 +1,10 @@
+package com.example.movieapp
+
+data class Movie (
+    var title: String = "Title",
+    var rating: Float = 2.5f,
+    var genre: String = "Genre",
+    var director: String = "Director",
+    var actors: String = "Actors",
+    var story: String = "Story Description"
+)
