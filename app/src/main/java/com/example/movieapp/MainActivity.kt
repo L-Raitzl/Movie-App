347a890b1ecd4d20e4a2c7aea9322108a5a36283
Lukas Raitzl <lukas.raitzl@gmail.com>
2021-03-10 20:06:03 +0100

    Learning Diary 2 - final commit

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -25,25 +25,5 @@ class MainActivity : AppCompatActivity() {
     private fun actionOnButton() {
         Toast.makeText(this, "You just pushed a Button! Nice.",
             Toast.LENGTH_SHORT).show()
-        bindStuff()
-    }
-
-    private fun bindStuff() {
-        binding. apply {
-            movie?.title = "Whiplash"
-            movie?.rating = 4f
-            movie?.genre = "Drama, Music"
-            movie?.director = "Damien Chazelle"
-            movie?.actors = "Miles Teller, J.K. Simmons"
-            movie?.story = "Andrew Neiman is a talented 19-year-old drummer and attends the (fictional) Shaffer Conservatory of Music in New York City. " +
-                           "One day he was discovered by the head of the studio band, Terence Fletcher. The band leader tries to spur his students on to top " +
-                           "performance through sadistic severity and verbal humiliation. Andrew is increasingly exposed to \"carrot and stick\" psychological violence. \n\n" +
-                           "Increasingly, he sees through the methods, but gets involved with them. He even separates from his girlfriend Nicole so that he can concentrate " +
-                           "exclusively on his music career. Due to Fletchers enormous pressure to be on time, Andrew causes a car accident and comes to a performance injured. " +
-                           "He cannot play in his condition and Fletcher throws him out of the band. Andrew freaks out, throws himself at Fletcher and beats him, whereupon he is " +
-                           "expelled from the Conservatory. \n\n Meanwhile, after the suicide of a former student, the supervisor learns of Fletchers degrading methods. Reluctantly, " +
-                           "Andrew is persuaded to testify anonymously against his teacher. Fletcher then loses his job."
-            invalidateAll()
-        }
     }
 }
--- app/src/main/java/com/example/movieapp/Movie.kt
+++ app/src/main/java/com/example/movieapp/Movie.kt
@@ -1,10 +1,17 @@
 package com.example.movieapp
 
 data class Movie (
-    var title: String = "Title",
-    var rating: Float = 2.5f,
-    var genre: String = "Genre",
-    var director: String = "Director",
-    var actors: String = "Actors",
-    var story: String = "Story Description"
+    var title: String = "Whiplash",
+    var rating: Float = 4.0f,
+    var genre: String = "Drama, Music",
+    var director: String = "Damien Chazelle",
+    var actors: String = "Miles Teller, J.K. Simmons",
+    var story: String = "Andrew Neiman is a talented 19-year-old drummer and attends the (fictional) Shaffer Conservatory of Music in New York City. " +
+            "One day he was discovered by the head of the studio band, Terence Fletcher. The band leader tries to spur his students on to top " +
+            "performance through sadistic severity and verbal humiliation. Andrew is increasingly exposed to \"carrot and stick\" psychological violence. \n\n" +
+            "Increasingly, he sees through the methods, but gets involved with them. He even separates from his girlfriend Nicole so that he can concentrate " +
+            "exclusively on his music career. Due to Fletchers enormous pressure to be on time, Andrew causes a car accident and comes to a performance injured. " +
+            "He cannot play in his condition and Fletcher throws him out of the band. Andrew freaks out, throws himself at Fletcher and beats him, whereupon he is " +
+            "expelled from the Conservatory. \n\n Meanwhile, after the suicide of a former student, the supervisor learns of Fletchers degrading methods. Reluctantly, " +
+            "Andrew is persuaded to testify anonymously against his teacher. Fletcher then loses his job."
 )
